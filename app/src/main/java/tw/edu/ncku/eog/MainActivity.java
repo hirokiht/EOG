6de7c1caca244bd64295a0857426d7c7f4730fa1
john <hirokiht@gmail.com>
2015-08-05 00:29:20 +0800

    Initial EOG Commit

---
+++ app/src/main/java/tw/edu/ncku/eog/MainActivity.java
@@ -0,0 +1,280 @@
+package tw.edu.ncku.eog;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothProfile;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ProgressBar;
+
+import org.jtransforms.fft.FloatFFT_1D;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
+
+
+public class MainActivity extends AppCompatActivity implements BleFragment.AdcListener, TimerFragment.OnTimerListener{
+    private final static int REQUEST_ENABLE_BT = 1;
+    private final static int BUFFER_SIZE = 128;
+    private final static int BEGIN_FREQ = 8, END_FREQ = 12;
+    private FragmentManager fragmentManager;
+    private BleFragment bleFragment;
+    private TimerFragment timerFragment;
+    private ActivityState state = ActivityState.ENABLE_BLE;
+    private short sampling_period = 7;
+    private Buffer dataBuffer;
+    private ProgressBar energyMeter;
+
+    @Override
+    public void onTimerStateChange(boolean started, boolean finished) {
+        state = finished? ActivityState.COMPLETE : started? ActivityState.BEGIN_TEST : ActivityState.READY;
+        checkState();
+    }
+
+    private enum ActivityState{
+        NO_BLE, ENABLE_BLE, SELECT_DEVICE, READY, BEGIN_TEST, COMPLETE
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fragmentManager = getFragmentManager();
+        setContentView(R.layout.activity_main);
+        bleFragment = (BleFragment) fragmentManager.findFragmentById(R.id.bleFragment);
+        timerFragment = (TimerFragment) fragmentManager.findFragmentById(R.id.fragment);
+        energyMeter = (ProgressBar) findViewById(R.id.energyMeter);
+        if(savedInstanceState != null && savedInstanceState.containsKey("state")) {
+            state = (ActivityState) savedInstanceState.getSerializable("state");
+            sampling_period = bleFragment.getSamplingPeriod();
+        }
+        checkState();
+        if(savedInstanceState == null && state == ActivityState.ENABLE_BLE)  //request bt if it is the first time starting this app
+            startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                    .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP), REQUEST_ENABLE_BT);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        finishActivity(REQUEST_ENABLE_BT);
+        outState.putSerializable("state", state);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, final int resultCode, Intent data){
+        if(requestCode == REQUEST_ENABLE_BT){
+            if(checkState() == ActivityState.ENABLE_BLE){    //wait for a while for BT to be enabled
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(!isChangingConfigurations() && !isDestroyed() && !isFinishing() &&
+                                checkState() == ActivityState.ENABLE_BLE ) {    //the wait is over
+                            state = ActivityState.NO_BLE;
+                            checkState();
+                        }
+                    }
+                }, 2000);
+            }else if (fragmentManager.findFragmentByTag("waitForBt") != null)
+                ((DialogFragment)fragmentManager.findFragmentByTag("waitForBt")).dismiss();
+        }
+    }
+
+    @Override
+    public void onSamplingPeriodChanged(short sampling_period) {
+        if(checkState() == ActivityState.READY && sampling_period != this.sampling_period)
+            bleFragment.saveSamplingPeriod(this.sampling_period);
+    }
+
+    @Override
+    public void onDataReceived(short data) {
+        if(dataBuffer == null || !(dataBuffer instanceof ShortBuffer))
+            return;
+        ((ShortBuffer)dataBuffer).put(data);
+        if(!dataBuffer.hasRemaining())
+            processBuffer();
+    }
+
+    @Override
+    public void onDataBufferReceived(byte[] buffer) {
+        if(dataBuffer == null || !(dataBuffer instanceof ByteBuffer))
+            return;
+        ((ByteBuffer)dataBuffer).put(buffer);
+        if(!dataBuffer.hasRemaining())
+            processBuffer();
+    }
+
+    @Override
+    public void onDataBufferReceived(short[] buffer) {
+        if(dataBuffer == null || !(dataBuffer instanceof ShortBuffer))
+            return;
+        ((ShortBuffer)dataBuffer).put(buffer);
+        if(!dataBuffer.hasRemaining())
+            processBuffer();
+    }
+
+    @Override
+    public void onConnectionStateChange(int newState) {
+        state = newState == BluetoothProfile.STATE_CONNECTED? ActivityState.READY : ActivityState.SELECT_DEVICE;
+        checkState();
+    }
+
+    private ActivityState checkState(){
+        switch(state){
+            case NO_BLE:
+                if (fragmentManager.findFragmentByTag("waitForBt") != null)
+                    ((DialogFragment)fragmentManager.findFragmentByTag("waitForBt")).dismiss();
+                if(fragmentManager.findFragmentByTag("NoBtAlertDialog") == null)
+                    new DialogFragment(){
+                        @Override
+                        public Dialog onCreateDialog(Bundle savedInstanceState) {
+                            this.setRetainInstance(true);
+                            AlertDialog dialog = new AlertDialog(getActivity()){
+                                @Override
+                                public void dismiss(){
+                                    this.setDismissMessage(null);
+                                    super.dismiss();
+                                }
+
+                                @Override
+                                public void cancel(){
+                                    super.cancel();
+                                    getActivity().finish();
+                                }
+                            };
+                            dialog.setTitle(R.string.need_bt);
+                            dialog.setMessage(getString(R.string.bt_not_found));
+                            dialog.setButton(DialogInterface.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener(){
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                            return dialog;
+                        }
+                    }.show(fragmentManager, "NoBtAlertDialog");
+                break;
+            case ENABLE_BLE:
+                timerFragment.reset();
+                if(BluetoothAdapter.getDefaultAdapter() == null){
+                    state = ActivityState.NO_BLE;
+                    checkState();
+                }else if(!BluetoothAdapter.getDefaultAdapter().isEnabled()){
+                    if(fragmentManager.findFragmentByTag("waitForBt") == null)
+                        new DialogFragment() {
+                            @Override
+                            public void onDestroyView() {   //Fix issue: https://code.google.com/p/android/issues/detail?id=17423
+                                if (getDialog() != null && getRetainInstance())
+                                    getDialog().setDismissMessage(null);
+                                super.onDestroyView();
+                            }
+
+                            @Override
+                            public Dialog onCreateDialog(Bundle savedInstanceState) {
+                                this.setRetainInstance(true);
+                                ProgressDialog dialog = new ProgressDialog(getActivity()){
+                                    @Override
+                                    public void dismiss() {
+                                        this.setDismissMessage(null);
+                                        super.dismiss();
+                                    }
+
+                                    @Override
+                                    public void cancel(){
+                                        super.cancel();
+                                        state = ActivityState.NO_BLE;
+                                        checkState();
+                                    }
+                                };
+                                dialog.setTitle(R.string.bt_not_enabled);
+                                dialog.setMessage(getString(R.string.wait_for_bt_enable));
+                                dialog.setIndeterminate(true);
+                                dialog.setCancelable(true);
+                                return dialog;
+                            }
+                        }.show(fragmentManager, "waitForBt");
+                }else {
+                    state = ActivityState.SELECT_DEVICE;
+                    return checkState();
+                }
+                break;
+            case SELECT_DEVICE:
+                timerFragment.reset();
+                if(!BluetoothAdapter.getDefaultAdapter().isEnabled()){
+                    state = ActivityState.ENABLE_BLE;
+                    return checkState();
+                }
+                if (fragmentManager.findFragmentByTag("waitForBt") != null)
+                    ((DialogFragment)fragmentManager.findFragmentByTag("waitForBt")).dismiss();
+                if(bleFragment != null && bleFragment.getDevice() != null) {
+                    state = ActivityState.READY;
+                    return checkState();
+                }
+                BleFragment.discoverDevices(this);
+                break;
+            case READY:
+                if(!BluetoothAdapter.getDefaultAdapter().isEnabled() || bleFragment == null || bleFragment.getDevice() == null){
+                    state = BluetoothAdapter.getDefaultAdapter().isEnabled()? ActivityState.SELECT_DEVICE : ActivityState.ENABLE_BLE;
+                    return checkState();
+                }
+                if(dataBuffer == null)
+                    (dataBuffer = ShortBuffer.allocate(BUFFER_SIZE)).limit(BUFFER_SIZE);
+                else dataBuffer.clear();
+                if(bleFragment.getSamplingPeriod() != sampling_period)
+                    bleFragment.saveSamplingPeriod(sampling_period);
+                timerFragment.reset(true);
+                bleFragment.setBuffered12bitAdcNotification(false);
+                break;
+            case BEGIN_TEST:
+                if(!BluetoothAdapter.getDefaultAdapter().isEnabled() || bleFragment == null || bleFragment.getDevice() == null){
+                    state = BluetoothAdapter.getDefaultAdapter().isEnabled()? ActivityState.SELECT_DEVICE : ActivityState.ENABLE_BLE;
+                    return checkState();
+                }
+                bleFragment.setBuffered12bitAdcNotification(true);
+                timerFragment.start();
+                break;
+            case COMPLETE:
+                if(!BluetoothAdapter.getDefaultAdapter().isEnabled() || bleFragment == null || bleFragment.getDevice() == null){
+                    state = BluetoothAdapter.getDefaultAdapter().isEnabled()? ActivityState.SELECT_DEVICE : ActivityState.ENABLE_BLE;
+                    return checkState();
+                }
+                bleFragment.setBuffered12bitAdcNotification(false);
+                break;
+        }
+        return state;
+    }
+
+    private void processBuffer(){
+        float[] data = new float[BUFFER_SIZE];
+        if(dataBuffer instanceof  ShortBuffer)
+            for(int i = 0 ; i < BUFFER_SIZE ; i++)
+                data[i] = ((ShortBuffer)dataBuffer).get(i)/4096f;
+        else if(dataBuffer instanceof ByteBuffer)
+            for(int i = 0 ; i < BUFFER_SIZE ; i++)
+                data[i] = (0xFF&((ByteBuffer)dataBuffer).get(i))/256f;
+        else throw new UnsupportedOperationException("Invalid buffer found when processing buffer!");
+        dataBuffer.clear();
+        Log.d("processBuffer", "start processing data...");
+        //TODO: apply median/mean filter to remove spikes.
+        FloatFFT_1D fft = new FloatFFT_1D(data.length);
+        fft.realForward(data);
+        float[] fftResult = new float[BUFFER_SIZE/2];
+        float sum = 0f, power = 0f;
+        for(int i = 0 ; i < data.length ; i+=2)
+            sum += fftResult[i/2] = data[i]*data[i]+data[i+1]*data[i+1];
+        int beginIndex = Math.round(BUFFER_SIZE*BEGIN_FREQ*sampling_period/1000f),
+                endIndex = Math.round(BUFFER_SIZE * END_FREQ * sampling_period / 1000f);
+        for(int i = beginIndex ; i < endIndex ; i++)
+            power += fftResult[i];
+        int ratio = (int)(power*100f/sum+0.5f); //ratio in percentage
+        energyMeter.setProgress(ratio);
+    }
+}
---
+++ app/src/main/java/tw/edu/ncku/eog/TimerFragment.java
@@ -0,0 +1,158 @@
+package tw.edu.ncku.eog;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.os.CountDownTimer;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.CompoundButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link TimerFragment.OnTimerListener} interface
+ * to handle interaction events.
+ */
+public class TimerFragment extends Fragment implements CompoundButton.OnCheckedChangeListener{
+    private ToneGenerator toneGenerator;
+    private ToggleButton testButton;
+    private TextView timerText;
+    private ProgressBar timerProgress;
+    private boolean start = false;
+    private CountDownTimer timer = new CountDownTimer(30000, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            timerText.setText(String.valueOf(millisUntilFinished/1000));
+            timerProgress.setProgress((int)millisUntilFinished/1000);
+        }
+
+        @Override
+        public void onFinish() {
+            start = false;
+            toneGenerator.startTone(ToneGenerator.TONE_PROP_ACK);
+            timerText.setTextColor(0xffff0000); //red
+            timerText.setText("0");
+            timerProgress.setProgress(0);
+            AlphaAnimation anim = new AlphaAnimation(0.0f, 1.0f);
+            anim.setDuration(40); //You can manage the blinking time with this parameter
+            anim.setStartOffset(160);
+            anim.setRepeatMode(Animation.REVERSE);
+            anim.setRepeatCount(5);
+            timerText.startAnimation(anim);
+            timerCallback.onTimerStateChange(false, true);
+        }
+    };
+
+    private OnTimerListener timerCallback;
+
+    public TimerFragment() {
+        this.setRetainInstance(true);
+        toneGenerator = new ToneGenerator(AudioManager.STREAM_NOTIFICATION,ToneGenerator.MAX_VOLUME);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt("progress", timerProgress.getProgress());
+        outState.putBoolean("btnEnabled", testButton.isEnabled());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_timer, container, false);
+        timerProgress = (ProgressBar) view.findViewById(R.id.progressBar);
+        timerText = (TextView) view.findViewById(R.id.textView);
+        testButton = (ToggleButton) view.findViewById(R.id.testButton);
+        if(savedInstanceState != null && savedInstanceState.containsKey("progress")){
+            int progress = savedInstanceState.getInt("progress",30);
+            timerProgress.setProgress(progress);
+            timerText.setText(String.valueOf(progress));
+            if(progress == 0)
+                timerText.setTextColor(0xffff0000); //red
+            testButton.setEnabled(savedInstanceState.getBoolean("btnEnabled", false));
+            testButton.setChecked(start);
+        }
+        testButton.setOnCheckedChangeListener(this);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            timerCallback = (OnTimerListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnTimerListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        timerCallback = null;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if(buttonView == testButton)
+            timerCallback.onTimerStateChange(isChecked, false);
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     */
+    public interface OnTimerListener {
+        void onTimerStateChange(boolean start, boolean finish);
+    }
+
+    public void start(){
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                testButton.setEnabled(true);
+            }
+        });
+        if(!start)
+            timer.start();
+        start = true;
+    }
+
+    public void reset(){
+        reset(false);
+    }
+
+    public void reset(final boolean ready){
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if(testButton != null){
+                    testButton.setEnabled(ready);
+                    testButton.setChecked(false);
+                }
+                if(timerText != null) {
+                    timerText.setTextColor(0xff000000); //BLACK
+                    timerText.setText(ready ? "30" : "-");
+                }
+            }
+        });
+        timerProgress.setProgress(30);
+        if(timer != null)
+            timer.cancel();
+        start = false;
+    }
+}
