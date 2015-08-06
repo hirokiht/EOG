ea6f26dd46320df4e140af422f2fe0e08de4da4e
john <hirokiht@gmail.com>
2015-08-06 22:57:56 +0800

    Changed alpha energyBar range to 0% to 10%

--- app/src/main/java/tw/edu/ncku/eog/GraphFragment.java
+++ app/src/main/java/tw/edu/ncku/eog/GraphFragment.java
@@ -104,7 +104,7 @@ public class GraphFragment extends Fragment {
             dataPoints[i] = new DataPoint((double)i/(samplingPeriod*2f*data.length),(double)data[i]/sum*100);
         final float maxRatio = max/sum*100;
         final double powerRatio = power/sum*100+0.5;
-        energyMeter.setProgress((int) powerRatio);
+        energyMeter.setProgress((int) powerRatio*10);   //set range is 0% to 10%
         handler.post(task = new Runnable() {
             @Override
             public void run() {
